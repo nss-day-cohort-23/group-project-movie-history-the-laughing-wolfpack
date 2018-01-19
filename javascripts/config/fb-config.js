92cd4a7c634e3af749f4db981dfe847b9265ec62
Eduardo DuQuesne <eduquesne@Eduardos-MacBook-Pro.local>
2018-01-19 15:16:25 -0600

    Firebase config and required modules

--- javascripts/config/fb-config.js
+++ javascripts/config/fb-config.js
@@ -1,1 +1,15 @@
 "use strict";
+
+const fbCreds = require("./fb-creds");
+
+const firebase = require("firebase/app");
+require("firebase/auth");
+
+const config = {
+  apiKey: fbCreds.apiKey,
+  authDomain: fbCreds.authDomain
+};
+
+firebase.initializeApp(config);
+
+module.exports = firebase;
